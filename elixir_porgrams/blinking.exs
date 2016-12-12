a16d492540a43a1676f705c7d837ae9f4ef96ac5
userimack <mahendra.k12@gmail.com>
2016-12-12 18:51:13 +0530

    Added files

---
+++ elixir_porgrams/blinking.exs
@@ -0,0 +1,20 @@
+defmodule Blink do
+  def blink() do
+    for i <- 1..60 do
+      IO.puts "Run time #{i}"
+      # {_,{_, m, _}} = :os.timestamp |> :calendar.now_to_datetime
+      
+      #if rem(i, 2) == 0 do
+       System.cmd("notify-send",["TODO","Blink"])
+       #end
+      Process.sleep(60000)
+       #:timer.sleep(60000)
+    end
+
+    blink() # Calling itself 
+  end
+
+end
+
+# Run using the command given below
+#  nohup elixir -r blinking.exs -e "Blink.blink" &
