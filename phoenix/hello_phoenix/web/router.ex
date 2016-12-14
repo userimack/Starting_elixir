3578c07c1f656d5c50f3c0e0b4049fefeed45e12
userimack <mahendra.k12@gmail.com>
2016-12-14 05:58:25 +0000

    Added files

--- phoenix/hello_phoenix/web/router.ex
+++ phoenix/hello_phoenix/web/router.ex
@@ -19,19 +19,30 @@ defmodule HelloPhoenix.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+    resources "/reviews", ReviewController
     resources "/users", UserController do
       resources "posts", PostController
     end
   end
  
- scope "admin" do
+ scope "admin", HelloPhoenix.Admin, as: :admin  do
    pipe_through :browser
   
-   resources "/reviews", HelloPhoenix.Admin.ReviewController
+   resources "/images", ImageController
+   resources "/reviews", ReviewController
+   resources "/users", UserController
  end
 
   # Other scopes may use custom stacks.
   # scope "/api", HelloPhoenix do
   #   pipe_through :api
   # end
+  scope "/api", HelloPhoenix.Api, as: : api do
+    pipe_through :api
+
+    scope "/v1", V1, as: :v1 do
+    resources "/images", ImageController
+    resources "/reviews", ReviewController
+    resources "/users", UserController
+   end
 end
